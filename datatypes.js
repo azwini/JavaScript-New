2232be87a3b74ad4789a22c005d6e8ebaa346468
Az <52291744+azwini@users.noreply.github.com>
2021-02-19 23:29:50 +0530

    Added java fundamentals
    added data types

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+let javascirptIsFun = true;
+console.log(javascirptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascirptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascirptIsFun = 'YES!';
+console.log(typeof javascirptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
