afaa246b44fd1bf37fe988f04b4a4e3fc158c163
Az <52291744+azwini@users.noreply.github.com>
2021-03-01 22:27:56 +0530

    Added javascript file
    added basic operator file

---
+++ basicOperators.js
@@ -0,0 +1,18 @@
+const now = 2021
+const age1 = now - 1992;
+const age2 = now - 2000;
+console.log(age1, age2);
+
+console.log(age1 * 2, age1 / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3, i.e, 2 * 2 * 2
+
+const firstName = 'Happy';
+const lastName = 'Birthday';
+console.log (firstName + ' ' + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100;
+x++; // x = x + 1 = 101;
+x--; // x = x - 1 = 100;
+console.log(x);
