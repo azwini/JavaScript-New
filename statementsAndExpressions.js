99c9551ac37502223965836cba3d2dfea74009cb
Az <52291744+azwini@users.noreply.github.com>
2021-04-12 22:02:34 +0530

    added part1 fundamentals
    added statement and expressions
    added switch, strict, ternary operators

---
+++ statementsAndExpressions.js
@@ -0,0 +1,6 @@
+if (23 > 10){
+    const str = '23 is bigger';
+}
+
+const me = 'Coder';
+console.log(`I'm ${2037 - 1992} years old ${me}`)
---
+++ ternaryOperator.js
@@ -0,0 +1,13 @@
+const age = 13;
+// age >= 18 ? console.log('I like to drink juice') :
+console.log('I like to drink water');
+
+let drink2;
+if (age >= 18){
+    drink2 = 'water';
+}
+else {
+    drink2 = 'juice';
+}
+console.log(drink2);
+console.log(`I like to drink ${age =>18 ? 'water' : `water`} `);
